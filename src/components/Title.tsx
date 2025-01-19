b582be79f6ba6a6641f00887e9511ca5c395091b
umiyuri777 <yuseiai15@gmail.com>
2025-01-20 00:54:12 +0900

    update: アイコンとタイトルを変更

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import { Text } from '@chakra-ui/react'
 const Title = () => {
   return (
     <Text fontSize="4xl" fontWeight="bold" textAlign="center" mt={4}>
-      Sanpo God
+      さんぽ神
     </Text>
   )
 }
